16ef4b9e246eb052244954136142c9b2ca1437b6
Daniel Bergløv <d.bergloev@mailbox.org>
2022-05-13 11:35:34 +0200

    Add version constraint for phar archives

--- stub.php
+++ stub.php
@@ -1,7 +1,10 @@
 <?php
 
 if (!defined('\im\IMPHP_BASE')) {
-    echo "Could not find imphp/base"; exit(1);
+    throw new Exception("Could not find imphp/base");
+
+} else if (version_compare(\im\IMPHP_BASE, ($v = '1.1.0'), '<')) {
+    throw new Exception("The package imphp/base-". \im\IMPHP_BASE ." is to old. At least $v is requried");
 }
 
 require "static.php";
